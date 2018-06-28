930e6b7133930b45c8fa88833031495ae4a7467e
Michael Weiss <mweiss9676@gmail.com>
2018-06-28 19:56:08 -0400

    parsed users search term and redirected to different api based on number of words

--- server.js
+++ server.js
@@ -1,53 +1,4 @@
-// const express = require('express');
-// const http = require('http');
-// const path = require('path');
 const fetch = require('node-fetch');
-// const socketIO = require('socket.io');
-
-// const app = express();
-// const server = http.Server(app);
-// const io = socketIO(server);
-// const port = process.env.PORT || 5000;
-
-// app.set('port', (port));
-// app.use('/public', express.static(__dirname + '/public'));
-
-
-// // app.get('/api/words', (req, res) => {
-// //   fetch('https://api.datamuse.com/words?rel_trg=cow&max=5')
-// //   .then(res => res.json())
-// //   .then(json => res.send(json))
-// // })
-// app.get('/', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'index.html'))
-//   .err(err => console.log(err))
-// });
-
-// app.get('/api/hello', (req, res) => {
-//   res.send( [{ 'word': 'Hello From Express' }, { 'word': 'second' }] );
-// });
-
-// server.listen(5000, () => console.log(`Listening on port ${5000}`));
-
-// io.on("connect", () => {
-  
-// })
-
-
-//   // componentDidMount() {
-//   //   fetch('https://api.datamuse.com/words?rel_trg=cow&max=5')
-//   //     .then(res => res.json())
-//   //     .then(data => this.setState({ response: data }))
-//   //     .then(() => window.fuckingwork = this.state.response)
-//   // }
-
-  
-
-//   // async componentDidMount(){
-//   //   const result = await fetch('/api/words')
-//   //   .then(() => this.setState({ response: result }))  
-//   // };
-
 
 const serverPort = 5000;
 const http = require("http");
@@ -55,57 +6,60 @@ const express = require("express");
 const app = express();
 const server = http.createServer(app);
 const WebSocket = require("ws");
-const websocketServer = new WebSocket.Server({ server });
+const wss = new WebSocket.Server({ server });
 
-websocketServer.on('connection', (webSocketClient) => {
+//what info does the server need to run the game?
+//all team names 
+//the game's keyword
+//the length of the rounds
+//the number of rounds
+//
+
+wss.on('connection', (webSocketClient) => {
     webSocketClient.send('{ "connection" : "ok"}');
-    webSocketClient.on('message', (message) => { websocketServer.clients.forEach( client => { client.send(`{ "message" : ${message} }`) });
+    webSocketClient.on('message', (message) => { wss.clients.forEach( client => { client.send(`{ "message" : ${message} }`) });
     });
 });
 
+const broadcast = (data, ws) => {
+  wss.clients.forEach(client => {
+    if (client.readyState === WebSocket.OPEN && client !== ws) {
+      client.send(JSON.stringify(data))
+    }
+  })
+}
+
+wss.on('connection', ws => {
+  let index
+})
+
 app.get('/api/:term', (req, res) => {
-  const term = req.params.term;
-  fetch(`https://api.datamuse.com/words?rel_trg=${term}&max=5`)
-  .then(terms => terms.json())
-  .then(json => res.send(json))
-  .catch(err => console.log(err));
+  let term = req.params.term;
+
+  if(term.includes(' ')) {
+
+    term = term.replace(' ', '+');
+    fetch(`https://api.datamuse.com/words?ml=${term}&max=5`)
+    .then(terms => terms.json())
+    .then(json => res.send(json))
+    .catch(err => console.log(err));
+    
+  } else {
+
+    fetch(`https://api.datamuse.com/words?rel_trg=${term}&max=5`)
+    .then(terms => terms.json())
+    .then(json => res.send(json))
+    .catch(err => console.log(err));
+  }
 })
 
 server.listen(serverPort, () => {console.log(`Websocket server started on port ` + serverPort) });
 
-// const express = require('express');
-// const fetch = require('node-fetch');
 
-// const app = express();
-// const port = process.env.PORT || 5000;
-// var result = [];
+//we could have a single user setup the game, entering in the length of rounds, number of rounds, and the keyword and then send the game state to this server. 
+//This server could then request each user input their team name
+//This server could then disseminate the game state to all connected users
+//That gamestate could be used to create identical games across all users
+//this server could then issue a command to begin round one and start a timer
+//when the timer ends the server could refuse any more answers (somehow)
 
-// app.get('/api/words', (req, res) => {
-//   fetch('https://api.datamuse.com/words?rel_trg=cow&max=5')
-//   .then(res => res.json())
-//   .then(json => res.send(json))
-// })
-
-
-// app.get('/api/hello', (req, res) => {
-//   res.send( [{ 'word': 'Hello From Express' }, { 'word': 'second' }] );
-// });
-
-
-
-// app.listen(port, () => console.log(`Listening on port ${port}`));
-
-
-  // componentDidMount() {
-  //   fetch('https://api.datamuse.com/words?rel_trg=cow&max=5')
-  //     .then(res => res.json())
-  //     .then(data => this.setState({ response: data }))
-  //     .then(() => window.fuckingwork = this.state.response)
-  // }
-
-  
-
-  // async componentDidMount(){
-  //   const result = await fetch('/api/words')
-  //   .then(() => this.setState({ response: result }))  
-  // };
